ea4e385d113cd6df550aa1a1929ebe1e1098bb21
dvher <diego.vergara5@mail.udp.cl>
2023-05-19 23:41:09 -0400

    Añadida solicitud de permisos camara

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Camera from 'react-native-vision-camera';
+import { useState } from 'react';
+
+const checkPermissions = async (state_func) => {
+    const statusCamera = await Camera.getCameraPermissionStatus();
+    if (statusCamera === 'not-determined') {
+        // Requesting the permission has just been initiated.
+        const newCameraPermissionStatus = await Camera.requestCameraPermission();
+        state_func(newCameraPermissionStatus);
+    } else {
+        // Permission was already granted or is undetermined.
+        state_func(statusCamera);
+    }
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [cameraPermissionStatus, setCameraPermissionStatus] = useState('not-determined');
+    checkPermissions(setCameraPermissionStatus);
+    return (
+        <View style={styles.container}>
+            <Text>Camera is {cameraPermissionStatus}</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
